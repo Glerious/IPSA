7db6811515d6e76ac72bfb0101acac30312c8b26
Glerious <jonascouturon2@gmail.com>
2023-10-25 14:57:36 +0200

    interface delete

--- Au311_Analyse_donnée/Calcul/main.py
+++ Au311_Analyse_donnée/Calcul/main.py
@@ -1,5 +1,15 @@
 from addons.BetterTkinter import *
 from addons.BetterText import *
 
-if __name__.__eq__("__main__"):
-    print("Nothing to display")
+fenetre = Interface("fenetre", "yellow", "300x300")
+fenetre.add(Volet(fenetre.get(), bg="blue"), "volet01h")
+
+volet01h: Volet = fenetre.to("volet01h")
+
+volet01h.add(Text(volet01h.get(), "Je ne sais pas", bg="yellow"))
+volet01h.add(Text(volet01h.get(), "En vrai oui", bg="red"))
+volet01h.add(Enter(volet01h.get(), "Entre de la merde..."))
+
+if __name__ == "__main__":
+    volet01h.addelement()
+    fenetre.show()
