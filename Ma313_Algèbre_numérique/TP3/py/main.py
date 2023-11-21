1db83f93dc5cf3ca6e83d48ffc9e89879c2856e7
Glerious <jonascouturon2@gmail.com>
2023-11-21 10:09:11 +0100

    upsate

--- Ma313_Algèbre_numérique/TP3/py/main.py
+++ Ma313_Algèbre_numérique/TP3/py/main.py
@@ -13,18 +13,7 @@ data1 = loadtxt("Ma313_Algèbre_numérique/TP3/py/ressources/TP3_partie1_donnees
 data2 = loadtxt("Ma313_Algèbre_numérique/TP3/py/ressources/TP3_partie1_donnees2.dat")
 
 def splitMatrix(matrix: ndarray):
-    """
-    Parameters
-    -------
-    matrix : Matrice de taille (n, p)
-
-    Return
-    -------
-    A_ : Matrice de taille (n, p) avec une première colomne de 1. Cette matrice reprend le paramètre matrix en laissant la dernière colomne.
-    b_ : Matrice de taille (n, 1) qui reprend le paramètre matrix en prenant la dernière colomne.
-    """
     return  c_[ones(matrix.shape[0]),matrix[:, :matrix.shape[1] - 1]] , matrix[:, matrix.shape[1] - 1:]
-
 
 # 1 . 1 . Resolution par la méthode des équation normale
 
@@ -37,7 +26,6 @@ def methodNormal(A: ndarray, b: ndarray):
     erreur = norm(A@x - b)
     return x, erreur
 
-
 # 1 . 2 . Resolution par la méthode QR
 
 # Méthode QR plus longue (note rendu)
@@ -48,13 +36,14 @@ def methodQR(A: ndarray, b: ndarray):
     erreur = norm(A@x -  b)
     return x, erreur
 
-# 1 . 2 . Résolution
+# 1 . 3 . Résolution
 
+# 1 . 3 . 1 Premier Jeu
 A1, b1 = splitMatrix(data1)
 x11, erreur11 = methodNormal(A1, b1)
 x12, erreur12 = methodQR(A1, b1)
 
-
+# 1 . 3 . 2 Deuxième Jeu
 A2, b2 = splitMatrix(data2)
 x21, erreur21 = methodNormal(A2, b2)
 x22, erreur22 = methodQR(A2, b2)
