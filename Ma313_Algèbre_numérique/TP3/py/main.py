4ed28c8a10ce094060ec167a67013689dceaec5b
Glerious <jonascouturon2@gmail.com>
2023-11-21 10:06:10 +0100

    functionisation

--- Ma313_Algèbre_numérique/TP3/py/main.py
+++ Ma313_Algèbre_numérique/TP3/py/main.py
@@ -13,20 +13,48 @@ data1 = loadtxt("Ma313_Algèbre_numérique/TP3/py/ressources/TP3_partie1_donnees
 data2 = loadtxt("Ma313_Algèbre_numérique/TP3/py/ressources/TP3_partie1_donnees2.dat")
 
 def splitMatrix(matrix: ndarray):
-    return  c_[ones(matrix.shape[0]),matrix[:, :2]] , matrix[:, 2:]
+    """
+    Parameters
+    -------
+    matrix : Matrice de taille (n, p)
 
-A, b = splitMatrix(data1)
+    Return
+    -------
+    A_ : Matrice de taille (n, p) avec une première colomne de 1. Cette matrice reprend le paramètre matrix en laissant la dernière colomne.
+    b_ : Matrice de taille (n, 1) qui reprend le paramètre matrix en prenant la dernière colomne.
+    """
+    return  c_[ones(matrix.shape[0]),matrix[:, :matrix.shape[1] - 1]] , matrix[:, matrix.shape[1] - 1:]
 
-# 1 . 1 Resolution par la méthode des équation normale
+
+# 1 . 1 . Resolution par la méthode des équation normale
 
 def normalEquation(A: ndarray, b:  ndarray):
     return A.T@A, A.T@b
 
-A_, b_ = normalEquation(A, b)
-x_1 = solve(A_, b_)
-erreur1 = norm(A@x_1 + b)
+def methodNormal(A: ndarray, b: ndarray):
+    A_, b_ = normalEquation(A, b)
+    x = solve(A_, b_)
+    erreur = norm(A@x - b)
+    return x, erreur
 
-# 1 . 2 Resolution par la méthode QR
 
-Q, R = qr(A)
-x_2 = solve(R, Q.T@b)
+# 1 . 2 . Resolution par la méthode QR
+
+# Méthode QR plus longue (note rendu)
+
+def methodQR(A: ndarray, b: ndarray):
+    Q, R = qr(A) # avec Q (n, p) et R (p, p) matrice carrée
+    x = solve(R, Q.T@b)
+    erreur = norm(A@x -  b)
+    return x, erreur
+
+# 1 . 2 . Résolution
+
+A1, b1 = splitMatrix(data1)
+x11, erreur11 = methodNormal(A1, b1)
+x12, erreur12 = methodQR(A1, b1)
+
+
+A2, b2 = splitMatrix(data2)
+x21, erreur21 = methodNormal(A2, b2)
+x22, erreur22 = methodQR(A2, b2)
