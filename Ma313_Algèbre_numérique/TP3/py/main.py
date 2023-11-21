f08133cc2d81341fc8c91c6b6df9a663fb0b9363
Glerious <jonascouturon2@gmail.com>
2023-11-21 09:34:45 +0100

    update

--- Ma313_Algèbre_numérique/TP3/py/main.py
+++ Ma313_Algèbre_numérique/TP3/py/main.py
@@ -1,13 +1,13 @@
 from numpy import loadtxt, ones, c_
 from numpy import ndarray
 
-from numpy.linalg import solve
+from numpy.linalg import solve, qr, norm
 
 
 point = loadtxt("Ma313_Algèbre_numérique/TP3/py/ressources/TP3_partie2_points.dat")
 
 
-# Régression linéaire multiple
+# 1 . Régression linéaire multiple
 
 data1 = loadtxt("Ma313_Algèbre_numérique/TP3/py/ressources/TP3_partie1_donnees1.dat")
 data2 = loadtxt("Ma313_Algèbre_numérique/TP3/py/ressources/TP3_partie1_donnees2.dat")
@@ -17,17 +17,16 @@ def splitMatrix(matrix: ndarray):
 
 A, b = splitMatrix(data1)
 
+# 1 . 1 Resolution par la méthode des équation normale
+
 def normalEquation(A: ndarray, b:  ndarray):
     return A.T@A, A.T@b
 
 A_, b_ = normalEquation(A, b)
+x_1 = solve(A_, b_)
+erreur1 = norm(A@x_1 + b)
 
-print(solve(A_, b_))
+# 1 . 2 Resolution par la méthode QR
 
-
-
-
-
-
-
-
+Q, R = qr(A)
+x_2 = solve(R, Q.T@b)
